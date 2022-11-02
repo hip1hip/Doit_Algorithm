15a8892266495613c3d58c89646509cc427133ce
hip1hip <honginpyo2100@gmail.com>
2022-11-02 17:36:39 +0900

    Commit

---
+++ chap02/IntArrayInit.java
@@ -0,0 +1,15 @@
+package chap02;
+
+public class IntArrayInit {
+
+	public static void main(String[] args) {
+		int[] a = {4,5,4,4,5};
+		
+	
+		for (int i = 0; i < a.length; i++)
+			System.out.println("a[" + i + "] = " + a[i] );
+		
+		
+	}
+
+}
